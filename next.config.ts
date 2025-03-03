c03b2493e311cb40c3294984c81e10f7e9f114d9
songbikes <song@asobi.tw>
2025-03-03 18:56:46 +1300

    fix render issues, remove experimental as I'm not using canary version of nextjs like the tutorial video does

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
-  experimental: {
-    dynamicIO: true,
-    authInterrupts: true,
-  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
