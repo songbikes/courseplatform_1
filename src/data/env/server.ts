5f4af612a93fc7bdf191a6999eeaa399c73a751a
songbikes <song@asobi.tw>
2025-03-04 22:17:10 +1300

    add Clerk wehbook secret

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -8,10 +8,12 @@ export const env = createEnv({
     DB_NAME: z.string().min(1),
     DB_HOST: z.string().min(1),
     CLERK_SECRET_KEY: z.string().min(1),
+    CLERK_WEBHOOK_SECRET: z.string().min(1),
   },
   experimental__runtimeEnv: process.env,
 });
-
+//這是一個用來讀取.env文件的文件 並由此文件來驗證.env文件中的值
+//如果值不對，將會拋出錯誤 比如說是不是string 或者是不是至少有個值
 //this file reads values from the .env file and then validate them
 //if they are not valid, it will throw an error
 //and then send the values to db.ts and drizzle.config.ts
