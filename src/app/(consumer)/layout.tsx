cea53f99a2eb455cf54c85c548295d95dd7e07d1
songbikes <song@asobi.tw>
2025-03-04 22:10:35 +1300

    update nav bar and its style

--- src/app/(consumer)/layout.tsx
+++ src/app/(consumer)/layout.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Link from "next/link";
 import { ReactNode, Suspense } from "react";
-import { SignedIn } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import "../globals.css";
+import ss from "./layout.module.scss";
 
 export default function ConsumerLayout(
   { children }: 
@@ -16,21 +18,38 @@ export default function ConsumerLayout(
 }
 
 function Navbar() {
-  return <header className="flex h-12 shadow bg-background z-10">
-    <nav className="flex gap-4 container" >
-      <Link className="mr-auto text-lg hover:underline px-2 flex items-center" href="/">
-        Web Dev Simplified
-      </Link>
-      <Suspense>
-        {/* <SignedIn> */}
-          <Link className=" hover:bg-accent/10 flex items-center px-2" href="/courses">
-            My Courses
-          </Link>
-          <Link className=" hover:bg-accent/10 flex items-center px-2" href="/purchases">
-            Purchase History
-          </Link>
-        {/* </SignedIn> */}
-      </Suspense>
-    </nav>
-  </header>
+  return <header>
+      <nav className={ss.container} >
+        <Link className={ss.link} href="/">
+          Web Dev Simplified
+        </Link>
+        <Suspense>
+          <SignedIn>
+            <Link className={ss.link} href="/admin">
+              Admin
+            </Link>
+            <Link className={ss.link} href="/courses">
+              My Courses
+            </Link>
+            <Link className={ss.link} href="/purchases">
+              Purchase History
+            </Link>
+            <div>
+              <UserButton appearance={
+                {elements: {
+                  userButtonAvatarBox: { width: "100%", height: "100%" },
+                }}
+              }/>
+            </div>
+          </SignedIn>
+        </Suspense>
+        <Suspense>
+          <SignedOut>
+              <button>
+                <SignInButton>Sign In</SignInButton>
+              </button>
+          </SignedOut>
+        </Suspense>
+      </nav>
+    </header>
 }
