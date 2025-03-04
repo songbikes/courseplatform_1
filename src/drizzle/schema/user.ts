b80da009534c68489f2d63fe4af0a1763963fa4c
songbikes <song@asobi.tw>
2025-03-04 22:18:31 +1300

    update commnets

--- src/drizzle/schema/user.ts
+++ src/drizzle/schema/user.ts
@@ -27,3 +27,6 @@ export const UserRelationships = relations(
 );
 
 //how users can interact with the contents or course products to be specific
+//定義用戶的基本訊息的表格，包括用戶的名字、郵箱、角色、頭像等
+//用戶的角色有兩種，分別是admin和user
+//這些會傳到schema.ts中 然後再傳到db.ts中
